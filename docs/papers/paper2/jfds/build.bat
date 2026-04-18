e27c90222fcbc5a335444e3992c35eb8616a430f
iAmGiG <gigachris1@hotmail.com>
2026-04-17 21:11:31 -0400

    docs: Remove obsolete paper variants
    - digital_finance/ (rejected by Springer, JRFM is current Paper 2 journal target)
    
    - paper2/jfds/ (obsolete 5-day sequential methodology, superseded by unified multi-day Paper 2 scope)
    
    Git history preserves both for reference.

--- docs/papers/paper2/jfds/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-pdflatex -interaction=nonstopmode Regan_Xie_JFDS.tex
-bibtex Regan_Xie_JFDS
-pdflatex -interaction=nonstopmode Regan_Xie_JFDS.tex
-pdflatex -interaction=nonstopmode Regan_Xie_JFDS.tex
-echo.
-echo Build complete! Check for citation errors above.
